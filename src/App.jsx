e289846f19ad0d8f35beca5391e6f2356a4773e9
Harshit Kumawat <harshitkumawat953@gmail.com>
2026-08-19 22:33:35 +0530

    Commit 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <h1>Welcome to My Store</h1>
+      <p>Our e-commerce website is under construction.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>My Store</h2>
+
+      <div>
+        <a href="/">Home</a>
+        <a href="/products">Products</a>
+        <a href="/cart">Cart</a>
+        <a href="/login">Login</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
